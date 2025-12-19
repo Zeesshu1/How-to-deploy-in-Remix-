0a9be225d24efec4a58931692645ad3845f2fc06
Md Zeeshan <91277213+Zeesshu1@users.noreply.github.com>
2025-12-19 17:07:39 +0530

    Update BaseToken.sol

--- BaseToken.sol
+++ BaseToken.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.so
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BaseToken is ERC20 {
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+}
